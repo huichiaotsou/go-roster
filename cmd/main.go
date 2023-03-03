8992b16f3c34507103d98641e4de744a28a2f195
huichiaotsou <huichiao.tsou@gmail.com>
2023-03-03 20:57:54 +0800

    rm errors

--- cmd/main.go
+++ cmd/main.go
@@ -5,27 +5,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/huichiaotsou/go-roster/pkg/handler"
-	"github.com/huichiaotsou/go-roster/pkg/repository"
 	"github.com/rs/cors"
 )
 
 func main() {
 	// initialize router
 	r := mux.NewRouter()
-
-	// initialize repository
-	repo := repository.NewRepository()
-
-	// initialize handler
-	h := handler.NewHandler(repo)
-
-	// set up routes
-	r.HandleFunc("/api/items", h.GetItems).Methods("GET")
-	r.HandleFunc("/api/items/{id}", h.GetItem).Methods("GET")
-	r.HandleFunc("/api/items", h.CreateItem).Methods("POST")
-	r.HandleFunc("/api/items/{id}", h.UpdateItem).Methods("PUT")
-	r.HandleFunc("/api/items/{id}", h.DeleteItem).Methods("DELETE")
 
 	// set up CORS
 	c := cors.New(cors.Options{
