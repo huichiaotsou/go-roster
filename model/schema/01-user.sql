0930a8a4484ce94897ed24d3a1c713ae34825cbc
huichiaotsou <huichiao.tsou@gmail.com>
2023-03-24 21:29:49 +0800

    delete func from user sql

--- model/schema/01-user.sql
+++ model/schema/01-user.sql
@@ -43,23 +43,3 @@ CREATE TABLE user_teams_perms (
 
     UNIQUE (user_id, team_id, perm_id)
 );
-
-
--- Define the functions table to store the functions of the users: Vox 1, Vox 2, KB 1, MD...
--- 1 user can take more than 1 function
--- function list is defined by the admin
-CREATE TABLE functions (
-    id          SERIAL      PRIMARY KEY,
-    func_name   VARCHAR(50) NOT NULL UNIQUE
-);
-
--- Define the user_funcs table to indicate WHO can be in charge of WHAT;
--- 1 user can be in charge of more than 1 instrument/function
--- (who can play what is defined by the admins)
--- user_func is defined by the admin
-CREATE TABLE user_funcs (
-    user_id    INT NOT NULL REFERENCES users(id),
-    func_id    INT NOT NULL REFERENCES functions(id),
-
-    UNIQUE (user_id, func_id)
-);
