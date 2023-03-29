0f4fa548572c0e88697eeb805e1e24f977790a52
huichiaotsou <huichiao.tsou@gmail.com>
2023-03-29 19:31:17 +0800

    check rows affected of UpdateIsSuperuser

--- model/superuser.go
+++ model/superuser.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func (db *Database) UpdateIsSuperuser(userID string, is_super_user bool) error {
 	stmt := `UPDATE users SET is_super_user = $1 WHERE id = $2`
-	_, err := db.Sqlx.Exec(stmt, is_super_user, userID)
+	res, err := db.Sqlx.Exec(stmt, is_super_user, userID)
 	if err != nil {
 		return fmt.Errorf("error while updating is_super_user = %v : %s", is_super_user, err)
+	}
+
+	affected, _ := res.RowsAffected()
+	if affected == 0 {
+		return fmt.Errorf("user not found")
 	}
 
 	return nil
